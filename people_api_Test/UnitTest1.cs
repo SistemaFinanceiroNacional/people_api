76457ddc0865f7854e32a163207869b3addf051b
unknown <ivanildojunior2319@gmail.com>
2023-07-10 18:40:41 -0300

    Tests

--- people_api_Test/UnitTest1.cs
+++ people_api_Test/UnitTest1.cs
@@ -37,10 +37,95 @@ namespace people_api_Test
                 Cpf.makeCpf(greaterThenElevenDigits);
             });
         }
+
         [Test]
         public void Test4()
         {
+            string invalidCharacterCpf = "123456789 2";
+            Assert.Throws<InvalidCharacterCpfException>(() =>
+            {
+                Cpf.makeCpf(invalidCharacterCpf);
+            });
+        }
 
+        [Test]
+        public void Test5()
+        {
+            string exempleCorrectCpfOne = "16912963099";
+            Assert.DoesNotThrow(() =>
+            {
+                Cpf.makeCpf(exempleCorrectCpfOne);
+            });
+        }
+        
+        [Test]
+        public void Test6()
+        {
+            string exempleCorrectCpfTwo = "45088273061";
+            Assert.DoesNotThrow(() =>
+            {
+                Cpf.makeCpf(exempleCorrectCpfTwo);
+            });
+        }
+
+        [Test]
+        public void Test7()
+        {
+            string exempleCorrectCpfThree = "89507632069";
+            Assert.DoesNotThrow(() =>
+            {
+                Cpf.makeCpf(exempleCorrectCpfThree);
+            });
+        }
+
+        [Test]
+        public void Test8()
+        {
+            string exempleCorrectCpfFour = "72350516040";
+            Assert.DoesNotThrow(() =>
+            {
+                Cpf.makeCpf(exempleCorrectCpfFour);
+            });
+        }
+        
+        [Test]
+        public void Test9()
+        {
+            string exempleInvalidCpfOne = "16912963097";
+            Assert.Throws<InvalidCpfException>(() =>
+            {
+                Cpf.makeCpf(exempleInvalidCpfOne);
+            });
+        }
+
+        [Test]
+        public void Test10()
+        {
+            string exempleInvalidCpfTwo = "45088273041";
+            Assert.Throws<InvalidCpfException>(() =>
+            {
+                Cpf.makeCpf(exempleInvalidCpfTwo);
+            });
+        }
+
+        [Test]
+        public void Test11()
+        {
+            string exempleInvalidCpfThree = "89507732069";
+            Assert.Throws<InvalidCpfException>(() =>
+            {
+                Cpf.makeCpf(exempleInvalidCpfThree);
+            });
+        }
+
+        [Test]
+        public void Test12()
+        {
+            string exempleInvalidCpfFour = "72356516040";
+            Assert.Throws<InvalidCpfException>(() =>
+            {
+                Cpf.makeCpf(exempleInvalidCpfFour);
+            });
         }
     }
 }
