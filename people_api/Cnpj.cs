465efed2723b6b31177bee0126fffb6488290073
unknown <ivanildojunior2319@gmail.com>
2023-07-11 18:41:03 -0300

    added cnpj

---
+++ people_api/Cnpj.cs
@@ -0,0 +1,74 @@
+﻿using System;
+public class Cnpj
+{
+    private string cnpj;
+
+    private Cnpj(string cnpj)
+    {
+        this.cnpj = cnpj;
+    }
+
+    public static Cnpj makeCnpj(string possibleCnpj)
+    {
+        if("".Equals(possibleCnpj))
+        {
+            throw new EmptyCnpjException();
+        }
+
+        if(possibleCnpj.Length < 14 || possibleCnpj.Length > 14)
+        {
+            throw new InvalidLengthCnpjException();
+        }
+
+        foreach (char c in possibleCnpj)
+        {
+            if (!(char.IsDigit(c)))
+            {
+                throw new InvalidCharacterCnpjException();
+            }
+        }
+
+        int[] cnpjNumbers = new int[(possibleCnpj.Length)];
+
+        for (int i = 0; i < (possibleCnpj.Length); i++)
+        {
+            cnpjNumbers[i] = int.Parse(possibleCnpj[i].ToString());
+        }
+
+        int checkDigitOne = int.Parse(possibleCnpj.Substring(12, 1));
+        int checkDigitTwo = int.Parse(possibleCnpj.Substring(13, 1));
+
+        int verifyOne = 0;
+        int verifyTwo = 0;
+
+        verifyOne = 5 * cnpjNumbers[0] + 4 * cnpjNumbers[1] + 3 * cnpjNumbers[2] + 2 * cnpjNumbers[3];
+        verifyOne += 9 * cnpjNumbers[4] + 8 * cnpjNumbers[5] + 7 * cnpjNumbers[6] + 6 * cnpjNumbers[7];
+        verifyOne += 5 * cnpjNumbers[8] + 4 * cnpjNumbers[9] + 3 * cnpjNumbers[10] + 2 * cnpjNumbers[11];
+        verifyOne = 11 - verifyOne % 11;
+        if(verifyOne >= 10)
+        {
+            verifyOne = 0;
+        }
+
+        verifyTwo = 6 * cnpjNumbers[0] + 5 * cnpjNumbers[1] + 4 * cnpjNumbers[2] + 3 * cnpjNumbers[3];
+        verifyTwo += 2 * cnpjNumbers[4] + 9 * cnpjNumbers[5] + 8 * cnpjNumbers[6] + 7 * cnpjNumbers[7];
+        verifyTwo += 6 * cnpjNumbers[8] + 5 * cnpjNumbers[9] + 4 * cnpjNumbers[10] + 3 * cnpjNumbers[11];
+        verifyTwo += 2 * cnpjNumbers[12];
+        verifyTwo = 11 - verifyTwo % 11;
+        if(verifyTwo >= 10)
+        {
+            verifyTwo = 0;
+        }
+
+        if((verifyOne != checkDigitOne) || (verifyTwo != checkDigitTwo))
+        {
+            throw new InvalidCnpjException();
+        }
+
+
+        return new Cnpj(possibleCnpj); 
+    }
+        
+}
+
+
---
+++ people_api_Test/CnpjTest.cs
@@ -0,0 +1,134 @@
+﻿namespace people_api_Test
+{
+    public class CnpjTest
+    {
+
+        [Test]
+        public void Test1()
+        {
+            string empty = "";
+
+            Assert.Throws<EmptyCnpjException>(() =>
+            {
+                Cnpj.makeCnpj(empty);
+            });
+        }
+
+        [Test]
+        public void Test2()
+        {
+            string lessThenFourteenDigits = "123";
+            Assert.Throws<InvalidLengthCnpjException>(() =>
+            {
+                Cnpj.makeCnpj(lessThenFourteenDigits);
+            });
+        }
+
+        [Test]
+        public void Test3()
+        {
+            string greaterThenFourteenDigits = "123456789101112131415";
+            Assert.Throws<InvalidLengthCnpjException>(() =>
+            {
+                Cnpj.makeCnpj(greaterThenFourteenDigits);
+            });
+        }
+
+        [Test]
+        public void Test4()
+        {
+            string invalidCharacterCnpj = "1111111111111 ";
+
+            Assert.Throws<InvalidCharacterCnpjException>(() =>
+            {
+                Cnpj.makeCnpj(invalidCharacterCnpj);
+            });
+        }
+
+        [Test]
+        public void Test5()
+        {
+            string exempleCorrectCnpjOne = "79592112000139";
+            Assert.DoesNotThrow(() =>
+            {
+                Cnpj.makeCnpj(exempleCorrectCnpjOne);
+            });
+
+        }
+
+        [Test]
+        public void Test6()
+        {
+            string exempleCorrectCnpjTwo = "54607642000182";
+            Assert.DoesNotThrow(() =>
+            {
+                Cnpj.makeCnpj(exempleCorrectCnpjTwo);
+            });
+
+        }
+
+        [Test]
+        public void Test7()
+        {
+            string exempleCorrectCnpjThree = "17065292000143";
+            Assert.DoesNotThrow(() =>
+            {
+                Cnpj.makeCnpj(exempleCorrectCnpjThree);
+            });
+
+        }
+
+        [Test]
+        public void Test8()
+        {
+            string exempleCorrectCnpjFour = "95544857000189";
+            Assert.DoesNotThrow(() =>
+            {
+                Cnpj.makeCnpj(exempleCorrectCnpjFour);
+            });
+
+        }
+
+        [Test]
+        public void Test9()
+        {
+            string exempleIncorrectCnpjOne = "79592002000139";
+            Assert.Throws<InvalidCnpjException>(() =>
+            {
+                Cnpj.makeCnpj(exempleIncorrectCnpjOne);
+            });
+        }
+
+        [Test]
+        public void Test10()
+        {
+            string exempleIncorrectCnpjTwo = "54377642000182";
+            Assert.Throws<InvalidCnpjException>(() =>
+            {
+                Cnpj.makeCnpj(exempleIncorrectCnpjTwo);
+            });
+        }
+
+        [Test]
+        public void Test11()
+        {
+            string exempleIncorrectCnpjThree = "17065122000143";
+            Assert.Throws<InvalidCnpjException>(() =>
+            {
+                Cnpj.makeCnpj(exempleIncorrectCnpjThree);
+            });
+        }
+
+        [Test]
+        public void Test12()
+        {
+            string exempleIncorrectCnpjFour = "95544857009989";
+            Assert.Throws<InvalidCnpjException>(() =>
+            {
+                Cnpj.makeCnpj(exempleIncorrectCnpjFour);
+            });
+        }
+
+
+    }
+}
