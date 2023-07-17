e204b0ace326e9fb8ce5735c978f7fb491ff7169
unknown <ivanildojunior2319@gmail.com>
2023-07-17 20:26:21 -0300

    Complete Person entity

---
+++ people_api/Empregado.cs
@@ -0,0 +1,16 @@
+﻿
+
+    public class Empregado : JobSituation
+{
+    private Empresa empresa { get; set; }
+
+    public Empregado(Empresa empresa)
+    {
+        this.empresa = empresa;
+    }
+
+    public string toString()
+    {
+        return "Funcionario da " + empresa;
+    }
+}
